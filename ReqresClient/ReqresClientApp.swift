edbd26f40b0aa3a9aa15c280c1d488fb8a36396a
Karpachov Illia <karpachov.i@ajax.systems>
2025-03-25 22:10:37 +0200

    Initial Commit

---
+++ ReqresClient/ReqresClientApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReqresClientApp.swift
+//  ReqresClient
+//
+//  Created by petro glyst on 25.03.2025.
+//
+
+import SwiftUI
+
+@main
+struct ReqresClientApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ReqresClientTests/ReqresClientTests.swift
@@ -0,0 +1,17 @@
+//
+//  ReqresClientTests.swift
+//  ReqresClientTests
+//
+//  Created by petro glyst on 25.03.2025.
+//
+
+import Testing
+@testable import ReqresClient
+
+struct ReqresClientTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
